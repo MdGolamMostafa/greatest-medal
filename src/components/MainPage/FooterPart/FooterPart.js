11eead1767a51ee2fbf016faefde3821ed978799
MdGolamMostafa <shakilhero11@gmail.com>
2021-01-04 10:01:32 +0600

    last check

--- src/components/MainPage/FooterPart/FooterPart.js
+++ src/components/MainPage/FooterPart/FooterPart.js
@@ -6,20 +6,20 @@ import './FooterPart.css';
 
 const FooterPart = () => {
     return (
-        <div className = 'd-flex justify-content-around footer-background-color pt-2 pb-5 m-2'>
-            <div className = 'd-flex justify-content-center flex-row'>
+        <div className = 'main-footer-design  pt-2 pb-5 m-2'>
+            <div className = 'flex-design'>
                 <img src={phones} alt=""/>
-                    <small className = 'flex-style text-white'>
+                    <small className = 'flex-style  text-white'>
                         Toll free<span className ='font-weight-bold'> 1800 200 1234</span>
                     </small>
             </div>
 
-            <div className = 'd-flex justify-content-center flex-row'>
+            <div className = 'flex-design'>
                 <img src={facebook} alt=""/>
                     <small className = 'flex-style text-white'>www.facebook.com/cripumps</small>
             </div>
 
-            <div className = 'd-flex justify-content-center flex-row'>
+            <div className = 'flex-design'>
                 <img src={web} alt=""/>
                     <small className = 'flex-style text-white'>www.crigroups.com</small>
             </div>
--- src/components/MainPage/ItemsPart/ItemsPart.js
+++ src/components/MainPage/ItemsPart/ItemsPart.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Raw from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import items from './items.png';
+
 const ItemsPart = () => {
     return (
         <Container className="mt-5" fluid>
@@ -11,10 +10,10 @@ const ItemsPart = () => {
                 <img className="w-75 h-75 d-flex justify-content-center" src={items} alt="Company logo"/>
             </div>
                 <h6 className = 'mt-3 d-flex justify-content-center '>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</h6>
-            <hr className ='mx-3 my-4 bg-danger'/>
-            <h6 className = "font-weight-bold text-center m-2">
-                C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS
-            </h6>
+                    <hr className ='mx-3 my-4 bg-danger'/>
+                <h6 className = "font-weight-bold text-center m-2">
+                    C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS
+                </h6>
             <h6 className = 'text-center m-4'>
                 CHEMICALS <span className = 'text-danger'> &#124;</span> PROCESS POWER WATER
                 <span className = 'text-danger'> &#124;</span> WASTE WATER OILS <span className = 'text-danger'> &#124;</span> GAS PHARMA 
